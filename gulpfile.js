773d49a498e703e80ec4ac38c502f844dafd3309
Ivar Nilsen <ivar.nilsen@sparebank1.no>
2016-10-09 13:10:58 +0200

    Rip out skrollr

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var gulp = require('gulp');
+
+gulp.task('javascript', function () {
+  var b = browserify({
+    entries: './js/app.js',
+    debug: true,
+    transform: [babelify]
+  });
+
+  return b.bundle()
+    .pipe(source('app.min.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('js/*.js', ['javascript']);
+});
